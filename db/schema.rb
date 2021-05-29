239c6c59c9050f659fee8c8ce26d7a079598ebc6
Colin Alexander <colintalex@gmail.com>
2021-05-29 03:41:08 -0600

    Schema update and migration for Fact

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_085633) do
+ActiveRecord::Schema.define(version: 2021_05_29_092637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "facts", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.float "lat"
+    t.float "lng"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
