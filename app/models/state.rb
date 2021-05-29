a6f7a3fe97de9fc460454c46fd251167c08ec5ff
Colin Alexander <colintalex@gmail.com>
2021-05-29 03:16:26 -0600

    Creates State model w/ validations

---
+++ app/models/state.rb
@@ -0,0 +1,8 @@
+class State < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :flag_image, presence: true
+    validates :population, presence: true
+    validates :capitol_name, presence: true
+    validates :population, presence: true
+end
