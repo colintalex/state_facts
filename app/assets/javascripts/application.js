7d28504c9d7a9a4e2f3960c3d809764fc5bbeda9
Colin Alexander <colintalex@gmail.com>
2021-05-30 03:17:40 -0600

    Adds buildMap after doc.ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require leaflet
+
+
+$(document).ready(function () {
+    buildMap(params = {})
+});
