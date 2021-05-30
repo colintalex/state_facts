92d346895ab1c1ab92b026b446aed88f9c07d627
Colin Alexander <colintalex@gmail.com>
2021-05-30 03:18:49 -0600

    Adds tileLayer to map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -33,6 +33,15 @@ function buildMap(params = {}) {
         preferCanvas: mapParams["preferCanvas"]
     });
 
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW50YWxleCIsImEiOiJja3BhdGEydTQwc2xzMm9vMWdhazg0ZmhuIn0.bog-VCj1Y2vySFOQ100GFw', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(map);
+
     if (mapParams["zoomHomeControl"])
         map.addControl(new L.Control.zoomHome());
 
