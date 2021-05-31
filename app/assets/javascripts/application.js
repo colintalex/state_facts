e43d2e0fb93bcf81a335b977d34cdb2fb38cb5c7
Colin Alexander <colintalex@gmail.com>
2021-05-31 00:07:00 -0600

    Implements simple marker for learning, temporary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,44 @@
 
 var activeStates = ['CO', 'NM', 'TX']
 
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
 $(document).ready(function () {
     buildMap(params = {})
+        
+    // Adds custom points to leaflet
+    // Temporary, will implement this inside ajax request
+    L.geoJSON(geojsonFeature, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, geojsonMarkerOptions);
+        },
+        onEachFeature: onEachFeature
+    }).addTo(map);
 });
