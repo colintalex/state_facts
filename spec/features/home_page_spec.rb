e047cbdc28187dc5eb065ee2b24b74dc3b2ece94
Colin Alexander <colintalex@gmail.com>
2021-05-31 00:07:13 -0600

    Creates skeleton HomePageFeature spec

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Home Page', type: :feature do 
+    scenario "When page loads" do
+        describe "Main Map" do
+            it "should only load the 2 available states" do
+                
+            end
+            
+        end
+
+        describe "Sidebar" do 
+            it "should load dropdown w/ 2 states, Search button and no other info" do
+
+            end
+        end
+    end
+
+
+    scenario "When first State is selected" do
+        describe "Main Map" do
+            it "should zoom/center selected state, no markers" do
+
+            end
+        end
+
+        describe "Sidebar" do
+            it "should show content for that state" do
+
+            end
+        end
+    end
+
+    scenario "When succeeding state(s) are selected" do
+        describe "Main Map" do
+            it "should clear markers and zoom/move to next state (same state selection applies" do
+
+            end
+        end
+
+        describe "Sidebar" do
+            it "should clear old information and replace with new" do
+                
+            end
+        end
+    end
+end
