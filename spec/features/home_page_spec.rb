07a6a65389a7accd89eb946599d0917f3fcdb84a
Colin Alexander <colintalex@gmail.com>
2021-05-31 04:14:15 -0600

    Adds feature specs to test for home page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Home Page', type: :feature do 
-    xscenario "When page loads" do
-        describe "Main Map" do
+        context "Main Map" do
             it "should only load the 2 available states" do
-                
+                visit '/'
+
+                expect(page).to have_current_path('/')
+                expect(page.has_css?('div#map')).to eql(true)
             end
-            
         end
 
-        describe "Sidebar" do 
+        context "Sidebar" do 
             it "should load dropdown w/ 2 states, Search button and no other info" do
+                visit '/'
 
+                expect(page.has_css?('#state-info-container'))
             end
         end
-    end
 
 
     xscenario "When first State is selected" do
-        describe "Main Map" do
+        context "Main Map" do
             it "should zoom/center selected state, no markers" do
 
             end
         end
 
-        describe "Sidebar" do
+        context "Sidebar" do
             it "should show content for that state" do
 
             end
@@ -32,13 +34,13 @@ RSpec.describe 'Home Page', type: :feature do
     end
 
     xscenario "When succeeding state(s) are selected" do
-        describe "Main Map" do
+        context "Main Map" do
             it "should clear markers and zoom/move to next state (same state selection applies" do
 
             end
         end
 
-        describe "Sidebar" do
+        context "Sidebar" do
             it "should clear old information and replace with new" do
 
             end
