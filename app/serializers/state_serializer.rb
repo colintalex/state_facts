a099344ce407b0eb1b65e67215fe8ea6c0c74886
Colin Alexander <colintalex@gmail.com>
2021-05-31 23:45:35 -0600

    Re-generated serializers, unusual errors were fixed

--- app/serializers/state_serializer.rb
+++ app/serializers/state_serializer.rb
@@ -1,4 +1,4 @@
 class StateSerializer < ActiveModel::Serializer
   has_many :facts
-  attributes :id, :name, :description, :flag_image, :capitol_name, :population
+  attributes :id, :name, :description, :capitol_name, :flag_image, :population
 end
