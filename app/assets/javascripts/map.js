aa6e34e50068eb906cb07307b834abc73e9a6bae
Colin Alexander <colintalex@gmail.com>
2021-05-31 23:46:05 -0600

    Updates Map copyright section to match provided img

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -41,7 +41,7 @@ function buildMap(params = {}) {
 
     // add visual tiles
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW50YWxleCIsImEiOiJja3BhdGEydTQwc2xzMm9vMWdhazg0ZmhuIn0.bog-VCj1Y2vySFOQ100GFw', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> | 2021 Sanborn. All rights reserved.',
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
