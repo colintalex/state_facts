65b469c7d1771a4da479c32c9f31692d4e681a51
Colin Alexander <colintalex@gmail.com>
2021-06-02 10:59:40 -0600

    Updates factories to be more realistic

--- spec/factories/facts.rb
+++ spec/factories/facts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :fact do
     title { "Colorado" }
-    details { "State details" }
-    lat { 1.1231235 }
-    lng { 1.53123321 }
+    details { "State details are somewhat longer so this about right" }
+    lat { 31.1231235 }
+    lng { -140.53123321 }
     association :state
   end
 end
--- spec/factories/states.rb
+++ spec/factories/states.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :state do
     name { 'Colorado' }
-    description { "MyText" }
+    description { "State description goes here, it can be lengthy so don't be afraid to add full sentences." }
     flag_image { "img/url" }
     capitol_name { "Denver" }
     population { 2500031 }
