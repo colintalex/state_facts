ddd367d18b7440c8b26e78ae6881aa3adf480158
Colin Alexander <colintalex@gmail.com>
2021-06-02 08:05:51 -0600

    Finalizes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,25 @@ puts "Seeding DB..."
     )
     CO.facts.create(
         title: "The Home of the Colorado Rockies is in historic Coors Field.",
-        details: "Built in 18328",
+        details: "Opened in 1995, and named after the Coors Brewing Co.",
         lat: 39.75567607730272,
         lng: -104.99419955953775
     )
     CO.facts.create(
         title: "Mount Evans is the Highest Mountain Peak in Colorado",
-        details: "14,271 ft above sea-level",
+        details: "Offers a seasonal drive up to 14,271 ft above sea-level",
         lat: 39.5889610668807,
         lng: -105.64404320421464
     )
     CO.facts.create(
         title: "Million Dollar Highway, Colorado's Most Expensive and Most Dangerous Road",
         details: "Road to Telluride, avg cost/mile was over 1 million dollars!",
+        lat: 37.831519351822315,
+        lng: -107.72417215942954
+    )
+    CO.facts.create(
+        title: "The largest single event in the entire state: the Colorado State Fair & Rodeo.",
+        details: "Occurs yearly in Pueblo, CO ever since 1872.",
         lat: 37.831519351822315,
         lng: -107.72417215942954
     )
@@ -43,20 +49,32 @@ puts "Seeding DB..."
         population: 2105005
     )
     NM.facts.create(
-        title: "Los Alamos",
-        details: "Original Construction Site for the Atomic Bombs during WWII",
+        title: "Original Construction Site for the Atomic Bombs during WWII",
+        details: "A classified location during the war, now called Los Alamos, NM.",
         lat: 35.88343763461308,
         lng: -106.30806560664591
     )
     NM.facts.create(
-        title: "Wheeler Peak",
-        details: "Highest Mountain in New Mexico, 13,167 ft above sea-level",
+        title: "Highest Mountain in New Mexico, 13,167 ft above sea-level",
+        details: "Wheeler Peak, named for Major George M. Wheeler, who surveyed the area during the 1870s.",
         lat: 36.55761242008017,
         lng: -105.41679801330899
     )
     NM.facts.create(
         title: "Billy the Kid’s Black Hill Hideout",
         details: "Used as hideout in 1880 for Billy and three members of his gang",
+        lat: 32.332083274067955, 
+        lng: -106.58768884490674
+    )
+    NM.facts.create(
+        title: "White Sands Missile Range, world’s first atomic bomb was detonation site",
+        details: "A very rare place to visit since it is only open one to the public day in April and one day in October.",
+        lat: 33.67745358919806,
+        lng: -106.47526564467894
+    )
+    NM.facts.create(
+        title: "New Mexico state capitol is a city unlike any other.",
+        details: "Founded in 1610, the city is the NM's 4th largest city.",
         lat: 32.332083274067955, 
         lng: -106.58768884490674
     )
