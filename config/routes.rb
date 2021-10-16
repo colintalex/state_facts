5fa9d01a1a95382ff125855da470d3c2a555eddc
Colin Alexander <colintalex@gmail.com>
2021-10-15 20:57:54 -0600

    Adds rocket engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RocketJobMissionControl::Engine => 'rocketjob'
+  
   get 'facts/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
