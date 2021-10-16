91607531eea037b30f7b41a4771497f293e70389
Colin Alexander <colintalex@gmail.com>
2021-10-15 20:58:14 -0600

    Initiates job from controllerf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @state_names = State.all.map{|s| {name: s.name, id: s.id}}
+    job = ReportJob.create!(run_at: 5.minutes.from_now)
     render :index
   end
 end
